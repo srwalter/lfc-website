32cf6b44ec806d5cc49f28ccbc2e7f763f75895c
Steven Walter <stevenrwalter@gmail.com>
2014-12-06 23:44:05 -0500

    Email sending with SendGrid works
    Requires auth parameters in Web.config or transform

---
+++ LFC/Controllers/EmailController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+
+using LFC.ViewModels;
+
+namespace LFC.Controllers
+{
+    [Authorize]
+    public class EmailController : Controller
+    {
+        // GET: Email
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index(EmailViewModel model)
+        {
+            var message = new MailMessage();
+            message.From = new MailAddress("info@lexingtonflyingclub.org", "Lexington Flying Club");
+            message.Subject = model.Subject;
+            var view = AlternateView.CreateAlternateViewFromString(model.Body, null, MediaTypeNames.Text.Plain);
+            message.AlternateViews.Add(view);
+
+            // XXX
+            message.To.Add("stevenrwalter@gmail.com");
+
+            var smtp = new SmtpClient();
+            try
+            {
+                smtp.Send(message);
+            }
+            catch (Exception e)
+            {
+                ViewBag.Message = "Failed to send: " + e.ToString();
+                return View(model);
+            }
+
+            return RedirectToAction("Index", "MembersArea");
+        }
+    }
+}
---
+++ LFC/ViewModels/EmailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LFC.ViewModels
+{
+    public class EmailViewModel
+    {
+        public String Subject { get; set; }
+        [DataType(DataType.MultilineText)]
+        public String Body { get; set; }
+    }
+}
