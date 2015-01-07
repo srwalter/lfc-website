cd1cc83555c39bca25e7ae60f3cbf6ec80cfadd4
Steven Walter <stevenrwalter@gmail.com>
2015-01-07 01:44:55 -0500

    wip: allow FlightLogID to be NULL
    Test to see if something is wrong with production database.
    It should be auto-assigning IDs

--- LFC/Models/FlightLog.cs
+++ LFC/Models/FlightLog.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LFC.Models
 {
     public class FlightLog
     {
+        [Key]
+        [DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.None)]
         public int FlightLogID { get; set; }
         [Required]
         [Display(Name = "Airplane")]
