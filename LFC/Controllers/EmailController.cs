fb26b3423f319b42125ef595c97cca5e56010ad8
Steven Walter <stevenrwalter@gmail.com>
2015-03-28 14:14:19 -0700

    Send a 30 day reminder for expiring badges

--- LFC/Controllers/EmailController.cs
+++ LFC/Controllers/EmailController.cs
@@ -18,6 +18,51 @@ namespace LFC.Controllers
     {
         private LFCContext db = new LFCContext();
 
+        private void SendBadgeReminders()
+        {
+            var body = "Your Airport Operation Area badge for Bluegrass Airport is scheduled to expire in the next 30 days.  Please ensure you renew it before it expires to avoid paying a penalty.";
+            var smtp = new SmtpClient();
+
+            var deadline = DateTime.Now.AddDays(30);
+            var users = db.Users.Where(x => (x.BadgeExpires ?? DateTime.MaxValue) == deadline).ToList();
+            foreach (var x in users)
+            {
+                var message = new MailMessage();
+                message.From = new MailAddress("info@lexingtonflyingclub.org", "Lexington Flying Club");
+                message.Subject = "LFC Airport Badge Expiring";
+
+                var view = AlternateView.CreateAlternateViewFromString(body, null, MediaTypeNames.Text.Plain);
+                message.AlternateViews.Add(view);
+
+                String pres = (from u in db.Users
+                               where u.Officer == ApplicationUser.OfficerTitle.President
+                               select u.Email).First();
+                String treasurer = (from u in db.Users
+                                    where u.Officer == ApplicationUser.OfficerTitle.Treasurer
+                                    select u.Email).First();
+                message.CC.Add(pres);
+                message.CC.Add(treasurer);
+                message.To.Add(x.Email);
+
+                ViewBag.Message += x.Email;
+                smtp.Send(message);
+            }
+        }
+
+        [AllowAnonymous]
+        public ActionResult SendTimedEmails()
+        {
+            try
+            {
+                SendBadgeReminders();
+            }
+            catch (Exception e)
+            {
+                ViewBag.Message = "Failed to send: " + e.ToString();
+            }
+            return View("Index");
+        }
+
         // GET: Email
 
         [Authorize(Roles = "Admin")]
